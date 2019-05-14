a2612ec8004243205ae1288fa2b67c1c16d14e6d
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-05-14 11:50:49 +0300

    Add files via upload

---
+++ Randevu Sistemi/Randevu Sistemi/Randevu Sistemi/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Randevu Sistemi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Randevu Sistemi")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("dba629d1-7aa4-461f-b712-ee2dbd520744")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
